ab96f6583fdc09a2df29471f6e0f42f2c43f5fab
Aslen420 <99992301+Aslen420@users.noreply.github.com>
2022-04-06 00:26:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,36 @@ client = commands.Bot(
     command_prefix='$', help_command=None, status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name="$help")
 )
 
+
 @client.event
 async def on_ready():
     print("Client is now active.")
 
+@client.event # CURRENTLY NOT WORKING
+async def on_guild_join(guild):
+    channel = client.get_channel(961053064150810675) #channel id here
+    await channel.send("```Joined Guild Name : {}\nGuild ID : {}```", member.guild.id, member.guild.name)
+@client.command()
+async def servers(guild):
+    channel = client.get_channel(961056847199092746)
+    activeservers = client.guilds
+    for guild in activeservers:
+        await channel.send("```{}```".format(guild.name))
+        print(guild.name)
+    await channel.send("***LIST COMPLETE ^ -----------------------------------------***")
+
+@client.command()
+async def feature_request(ctx, *, args):
+    channel = client.get_channel(961050242604740629) #channel id here
+    await channel.send("Recommendation : \n```{}```".format(args))
+
+    
 @client.command()
 async def help(ctx):
     await ctx.send("$add : Addition Command - Two Arguments")
     await ctx.send("$definition : Grab the definition of a word.")
+    await ctx.send("$translate : Translate a message into a selected language!")
+
 
 @client.command()
 async def add(ctx, arg1, arg2):
@@ -39,29 +61,37 @@ async def definition(ctx, arg):
         print("Test completed.")
 
 @client.command()
+async def serverlist(ctx):
+    await ctx.send("I'm in " + str(len(client.guilds)) + " servers!")
+
+@client.command()
 async def translate(ctx, *, args):
     def check(message: discord.Message):
         return message.channel == ctx.channel and message.author != ctx.me
     await ctx.send("What language would you like to translate to? type 'list' for all languages.")
     lang = await client.wait_for("message", check=check)
+    
     langformat = format(lang.content)
     if langformat == "french":
-        french = GoogleTranslator(source='auto', target='fr').translate(args)
-        await ctx.send(french)
+        lang = GoogleTranslator(source='auto', target='fr').translate(args)
+        await ctx.send(lang)
     elif langformat == "latin":
-        latin = GoogleTranslator(source='auto', target='la').translate(args)
-        await ctx.send(latin)
+        lang = GoogleTranslator(source='auto', target='la').translate(args)
+        await ctx.send(lang)
     elif langformat == "russian":
-        russian = GoogleTranslator(source='auto', target='ru').translate(args)
-        await ctx.send(russian)
+        lang = GoogleTranslator(source='auto', target='ru').translate(args)
+        await ctx.send(lang)
     elif langformat == "german":
-        german = GoogleTranslator(source='auto', target='de').translate(args)
-        await ctx.send(german)
+        lang = GoogleTranslator(source='auto', target='de').translate(args)
+        await ctx.send(lang)
     elif langformat == "arabic":
-        arabic = GoogleTranslator(source='auto', target='ar').translate(args)
-        await ctx.send(arabic)
+        lang = GoogleTranslator(source='auto', target='ar').translate(args)
+        await ctx.send(lang)
+    elif langformat == "english":
+        lang = GoogleTranslator(source='auto', target='en').translate(args)
+        await ctx.send(lang)
     elif langformat == "list":
-        await ctx.send("french, latin, russian, german, arabic")
+        await ctx.send("french, latin, russian, german, arabic, english")
     else:
         await ctx.send("Incorrect usage.")
 
